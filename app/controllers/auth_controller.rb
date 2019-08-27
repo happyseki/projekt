37698b97835a1d91962d797969ea67ecd6180cf2
Flatiron School <flatironschool@Flatirons-MacBook-Air-9.local>
2019-08-27 16:05:30 -0400

    jwt installed

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,15 @@ class AuthController < ApplicationController
   def login
     #params:{username:'', password:''}
     #find user by their username
+    user = User.find_by(username: params['username'])
     #authenticate the user
-    
+    is_authenticated = user.authenticate(params['password'])
+    puts "Hey, #{is_authenticated}"
+    if is_authenticated
+      render json: user
+    else
+      render json: { error: "wrong username or password" }
+    end
+    # log in the user
   end
 end
